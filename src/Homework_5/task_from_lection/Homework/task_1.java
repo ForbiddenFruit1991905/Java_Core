0715f9ef959a882359f4bbbdde1763b4c9a48801
Лихачёва Виктория Михайловна <29436838@s.masi.ru>
2024-10-21 23:47:54 +0300

    add task_1(HW)

--- src/Homework_5/task_from_lection/Homework/task_1.java
+++ src/Homework_5/task_from_lection/Homework/task_1.java
@@ -1,4 +1,34 @@
 package Homework_5.task_from_lection.Homework;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class task_1 {
+    public static void main(String[] args) throws IOException {
+        /*
+         Написать функцию, создающую резервную копию всех файлов в директории (без
+        поддиректорий) во вновь созданную папку ./backup
+         */
+        createBackup("./Homework_5");
+    }
+
+    public static void createBackup(String directoryPath) throws IOException {
+        Path backupDir = Paths.get(directoryPath, "backup");
+        Files.createDirectories(backupDir);
+
+        for (Path file : Files.list(Paths.get(directoryPath)).toList()) {
+            if (Files.isRegularFile(file)) {
+                try {
+                    Files.copy(file, backupDir.resolve(file.getFileName()));
+                    System.out.printf("Создана резервная копия файла: %s%n", file.getFileName());
+                } catch (IOException e) {
+                    throw new IOException(e);
+                }
+            }
+        }
+        System.out.println("Резервные копии файлов успешно созданы в папке 'backup'.");
+    }
+
 }
